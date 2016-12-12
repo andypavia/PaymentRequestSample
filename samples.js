4f4e0fdbf292dbf3b6f00d99f045d41b55ee2195
andypavia <pavia.andres@gmail.com>
2016-12-13 04:57:51 +0800

    fixing small typo

--- samples.js
+++ samples.js
@@ -88,7 +88,7 @@ Global.startPaymentRequestDynamicShipping = function()	{
 		return result.complete('success');
 	});
 
-	//Listens to a shipping address change
+	//Listen to a shipping address change
 	paymentRequest.addEventListener('shippingaddresschange', changeEvent => {
 		changeEvent.updateWith(new Promise(resolve => {
 			onShippingAddressChange(paymentRequest);
@@ -96,7 +96,7 @@ Global.startPaymentRequestDynamicShipping = function()	{
 		}));
 	});
 
-	//Listens to a shipping option change
+	//Listen to a shipping option change
 	paymentRequest.addEventListener('shippingoptionchange', changeEvent => {
 		changeEvent.updateWith(new Promise(resolve => {
 			onShippingOptionChange(paymentRequest);
