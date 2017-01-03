cd22fc72b37ab1385d82b002d9fcfced811f07ed
Andy Pavia <pavia.andres@gmail.com>
2017-01-03 14:36:32 -0800

    fixing btn to event mapping

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ window.onload = function() {
   requestContact.innerHTML = Global.startPaymentRequestWithContactInfo.toString();
 
   //attaching event listeners
-  staticShippingBtn.addEventListener('click', Global.startPaymentRequestWithContactInfo);
-  dynamicShippingBtn.addEventListener('click', Global.startPaymentRequestDigitalMerchandise);
-  noShippingBtn.addEventListener('click', Global.startPaymentRequestDynamicShipping);
-  requestContactBtn.addEventListener('click', Global.startPaymentRequestStaticShipping);
+  staticShippingBtn.addEventListener('click', Global.startPaymentRequestStaticShipping);
+  dynamicShippingBtn.addEventListener('click',Global.startPaymentRequestDynamicShipping);
+  noShippingBtn.addEventListener('click', Global.startPaymentRequestDigitalMerchandise);
+  requestContactBtn.addEventListener('click', Global.startPaymentRequestWithContactInfo);
 
   //Hide demo buttons if the browser doesn't support the Payment Request API
   if (!('PaymentRequest' in window)) {
-    notSupportedMessage.innerHTML = 'This browser does not support web payments. You should try Microsoft Edge.'
+    notSupportedMessage.innerHTML = 'This browser does not support web payments. You should try the Microsoft Edge browser!'
     buttons = document.getElementsByTagName("button");
     for (i = 0; i < buttons.length; i++) {
       buttons[i].disabled = true
